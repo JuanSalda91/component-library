74485cd0402ccd28e59e68a2aa464ab34190b963
JuanSalda91 <juansalda9131@gmail.com>
2025-12-12 00:26:23 -0500

    init component imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-
+import { UserProfileCard } from './components/UserprofileCard/UserProfileCard';
+import { AlertBox } from './components/AlertBox/AlertBox'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-
-    </>
+  <div>
+  </div>
   )
 }
 
