3ccb6f6af08b8091ce311c5afb20c35759b64779
JuanSalda91 <juansalda9131@gmail.com>
2025-12-12 21:30:02 -0500

    App.tsx init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import { useState } from 'react'
+import type { User, Product } from './components/types/index.ts'
 import { UserProfileCard } from './components/UserProfileCard/UserProfileCard';
+import { ProductDisplay } from './components/ProductDisplay/ProductDisplay';
 import { AlertBox } from './components/AlertBox/AlertBox'
 import './App.css'
 
 function App() {
+  const [showAlert, setShowAlert] = useState(false);
+  const [cartCount, setCartCount] = useState(0);
+
+  // -- Sample Data --- //
+  const currentUser: User = {
+    id: '',
+    name: '',
+    email: '',
+    role: '',
+    avatarUrl: '',
+  };
 
   return (
   <div>
